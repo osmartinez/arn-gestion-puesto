b6a30bf592fd6b5f2033a769c1a395c6a15187c4
oscar <oscarmtzz22@gmail.com>
2020-05-21 10:51:20 +0200

    Cambio visual mdb

--- config.js
+++ config.js
@@ -10,6 +10,11 @@ var config = {
             host: '192.168.0.104',
             port: '3000'
         }
+    },
+    puesto:{
+        codSeccion: '080',
+        idMaquina: 570,
+        rendimientoIdeal: 100,
     }
 };
 module.exports = config;
