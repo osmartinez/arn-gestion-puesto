25df0abea1c279f36fe5e04b4bf00466be55244d
oscar <oscarmtzz22@gmail.com>
2020-05-21 16:35:24 +0200

    Implemente selector slider para ritmo de maquina en settings

--- src/public/js/settings/main.js
+++ src/public/js/settings/main.js
@@ -1,22 +1,27 @@
 $(document).ready(function () {
     $('.stepper').mdbStepper();
     $('.mdb-select').materialSelect();
+    const $valueSpan = $('#label-ritmo-ideal');
+    const $value = $('#input-ritmo-ideal');
+    $valueSpan.html($value.val());
+    $value.on('input change', () => {
+        $valueSpan.html($value.val());
+    });
     Keyboard.init();
 })
 
 $('#select-secciones').change(function () {
     const codSeccion = $(this).find(":selected").val()
+    $('#select-maquinas').html('')
     $.ajax({
         url: `/dashboard/settings/maquinasEnSeccion/${codSeccion}`,
         dataType: 'json'
     }).done((maquinas) => {
-        if(maquinas){
-            for(const maquina of maquinas){
+        if (maquinas) {
+            for (const maquina of maquinas) {
                 $('#select-maquinas').append(`<option value="${maquina.ID}">${maquina.Nombre}</option>`)
             }
         }
-
-        
     }).fail((err) => {
         console.log(err)
     })
