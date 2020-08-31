a073be53b96e6a6b5ec50ddf45a61161222f4343
oscar <oscarmtzz22@gmail.com>
2020-08-31 16:53:15 +0200

    Añade funcion visual para activar/desactivar incidencias

--- src/public/js/settings/paso-3.js
+++ src/public/js/settings/paso-3.js
@@ -20,19 +20,32 @@ function armarTablaIncidencias() {
             columnaActual = 0;
         }
 
- 
+
 
     }
 
-    if(celdas != ''){
+    if (celdas != '') {
         let fila = `<tr id="fila-${filaActual}">${celdas}</tr>`
         $('#tabla-incidencias').append(fila)
     }
+
+    for(const incidencia of puesto.incidencias){
+        $(`#incidencia-${incidencia.Numero}`).click(function(){
+            if( $(`#incidencia-${incidencia.Numero}`).hasClass('btn-warning')){
+                $(`#incidencia-${incidencia.Numero}`).removeClass('btn-warning')
+                $(`#incidencia-${incidencia.Numero}`).addClass('btn-success')
+            }
+            else{
+                $(`#incidencia-${incidencia.Numero}`).addClass('btn-warning')
+                $(`#incidencia-${incidencia.Numero}`).removeClass('btn-success')
+            }
+        })
+    }
 }
 
-$('#btn-add-incidencia').click(function(){
+$('#btn-add-incidencia').click(function () {
     puesto.addIncidencia({
-        Numero: puesto.incidencias.length+1,
+        Numero: puesto.incidencias.length + 1,
         Nombre: $('#input-incidencia').val()
     })
     $('#input-incidencia').val('')
