2983029543a21432f489cdb50d9359617d430e6a
oscar <oscarmtzz22@gmail.com>
2020-10-08 17:31:22 +0200

    fix bug fechas utc

--- src/lib/model/puestoTareasActuales.model.js
+++ src/lib/model/puestoTareasActuales.model.js
@@ -8,7 +8,7 @@ const puestoTareasActualesSchema = new Schema({
     _id: mongoose.Schema.Types.ObjectId,
     puesto: {type: Puesto},
     tareas: {type: [Tarea]},
-    fechaInicio:{type: Date, default: new Date.now},
+    fechaInicio:{type: Date, default: Date.now},
     fechaFin:{type: Date,default:null},
     terminado: {type:Boolean, default:false}
 })
