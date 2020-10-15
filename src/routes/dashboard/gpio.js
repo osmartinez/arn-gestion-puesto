b2498ed91f2a2954e7fcbbede3d12641b9f5e0c0
oscar <oscarmtzz22@gmail.com>
2020-10-15 12:33:50 +0200

    intento fix bug pulsos fantasma

--- src/routes/dashboard/gpio.js
+++ src/routes/dashboard/gpio.js
@@ -1,9 +1,11 @@
 const device = process.env.DEVICE_ENV ||'raspi'
-
+var GpioConfiguracion = null
+if(device == 'raspi'){
+    GpioConfiguracion = require('../../lib/pins/gpio.config')
+}
 module.exports = function (router) {
     router.post('/gpio/obtenerEstadoPins', async (req, res) => {
         if(device == 'raspi'){
-            const GpioConfiguracion = require('../../lib/pins/gpio.config')
             GpioConfiguracion.refrescarValoresLectura()
             res.json(GpioConfiguracion.PINS)
         }
