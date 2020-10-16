470ce04ff121c225af63bea14bbd5f70244fadf9
oscar <oscarmtzz22@gmail.com>
2020-10-16 10:12:55 +0200

    fix bug

--- src/public/js/tarea/main.js
+++ src/public/js/tarea/main.js
@@ -220,7 +220,7 @@ setInterval(() => {
                                     }
                                 }
                             })
-                            break
+                            
                         }
                     }
                     else {
