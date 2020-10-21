a79902e059ac43ee44253ce44149a85982369849
oscar <oscarmtzz22@gmail.com>
2020-10-21 11:55:26 +0200

    cambia debouncetimeout

--- src/lib/pins/gpio.config.js
+++ src/lib/pins/gpio.config.js
@@ -31,7 +31,7 @@ function configurarPuesto(puesto) {
                 PINS[maquina.PinPulso].type = 'main-pulse'
 
                 try {
-                    PINS[maquina.PinPulso].gpio_object = new Gpio(PINS[maquina.PinPulso].number, PINS[maquina.PinPulso].mode, 'falling', { debounceTimeout: 500 })
+                    PINS[maquina.PinPulso].gpio_object = new Gpio(PINS[maquina.PinPulso].number, PINS[maquina.PinPulso].mode, 'falling', { debounceTimeout: 50 })
                     PINS[maquina.PinPulso].gpio_object.watch((err, value) => {
                         if (err) {
                             console.error(err)
